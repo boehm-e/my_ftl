a79e868f73ef8ecb60c25d716e7edcd9371caaab
erwan <boehm_e@etna-alternance.net>
2016-11-07 14:08:02 +0100

    etape5

--- etape_1/air_shed.c
+++ etape_1/air_shed.c
@@ -4,6 +4,7 @@
 t_ship *create_ship() {
   t_ship *ship = malloc(sizeof(t_ship));
   ship->weapon = NULL;
+  ship->drive = NULL;
   ship->hull = 50;
   if (ship) {
     return ship;
@@ -29,11 +30,28 @@ int add_weapon_to_ship(t_ship *ship) {
   }
 }
 
+int add_ftl_drive_to_ship(t_ship *ship) {
+  my_putstr(ADD_DRIVE);
+  t_ftl_drive *drive;
+  drive = malloc(sizeof(t_ftl_drive));
+  drive->energy = 10;
+  drive->system_state = my_strdup("online");
+  ship->drive = drive;
+  if (drive == ship->drive) {
+    my_putstr(ADD_DRIVE_SUCCESS);
+    return 1;
+  } else {
+    my_putstr(ADD_DRIVE_FAIL);
+    return 0;
+  }
+}
+
 int main(char *argc, char *argv) {
   my_putstr(SHIP_BUILDING);
   t_ship *ship;
   ship = create_ship();
   my_putstr(SHIP_WELDING_FINISH);
   add_weapon_to_ship(ship);
+  add_ftl_drive_to_ship(ship);
   return 0;
 }
--- etape_1/headers/strings.h
+++ etape_1/headers/strings.h
@@ -2,7 +2,12 @@
 #define SHIP_BUILDING "construction du vaisseau en cours...\n"
 #define SHIP_BUILDING_ERROR "le vaisseau n'a pas pu etre construit par manque de materiaux\n"
 #define SHIP_WELDING_FINISH "amelioration du vaisseau termine!\n"
+
 #define ADD_WEAPONS "ajout des armes en cours...\n"
 #define ADD_WEAPONS_FAIL "votre vaisseau a explose quand vous avez tente d'ajouter les armes\n"
 #define ADD_WEAPONS_SUCCESS "les armes on ete ajoutes avec succes!\n"
+
+#define ADD_DRIVE "ajout du reacteur en cours...\n"
+#define ADD_DRIVE_FAIL "votre vaisseau a explose lorsque vous avez pose le reacteur\n"
+#define ADD_DRIVE_SUCCESS "le reacteur a ete ajoute avec succes!\n"
 #endif
